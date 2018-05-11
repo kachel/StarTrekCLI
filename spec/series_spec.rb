806cd5f67df39f086a48be0994825d2075d9bb3c
Kachel Wittig <kjwittig@gmail.com>
2018-05-11 16:32:46 -0500

    excersice Series#seasons

--- spec/series_spec.rb
+++ spec/series_spec.rb
@@ -16,4 +16,18 @@ describe Series do
     expect { series.name = "Empire Strikes Back" }.to raise_error NoMethodError
   end
 
+  describe "#seasons" do
+      let(:series) { Series.new("Voyager") }
+
+    it "can be called" do
+      expect { series.seasons }.not_to raise_error NoMethodError
+    end
+
+    it "does return nil if there are no seasons" do
+      expect(series.seasons).to be_nil
+    end
+
+
+  end # Series#seasons
+
 end
