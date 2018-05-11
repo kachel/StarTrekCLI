7667c2bb8ad6064bb51cf3108aa0478a45313b0b
Kachel Wittig <kjwittig@gmail.com>
2018-05-11 16:07:54 -0500

    test series's name cannot be changed

--- spec/series_spec.rb
+++ spec/series_spec.rb
@@ -1,9 +1,19 @@
 require_relative "../lib/StarTrekCLI/series.rb"
 
-describe 'Series' do
-  describe '::new' do
-    it 'gets initialized with a name' do
-      expect{Series.new("Voyager")}.to_not raise_error
-    end
+describe Series do
+
+  let(:series) { Series.new("Voyager") }
+
+  it "can initialize a series" do
+    expect(series).to be_a(Series)
   end
+
+  it "initializes with a name" do
+    expect(series.name).to eq("Voyager")
+  end
+
+  it "does not allow you to change its name" do
+    expect { series.name = "Empire Strikes Back" }.to raise_error NoMethodError
+  end
+
 end
