d9c40d88424dd3db9ad39d00c311ede04221db0a
Kachel Wittig <kjwittig@gmail.com>
2018-05-25 22:37:10 -0500

    add episode nokogiri

--- lib/StarTrekCLI/scraper.rb
+++ lib/StarTrekCLI/scraper.rb
@@ -6,7 +6,8 @@ require 'rb-readline'
 module StarTrekCLI
   class Scraper
 
-    def each_table_cell
+    # This method pulls information from the Chakoteya for each Star Trek series using an iterator. The data is constructed as a hash with `image_url`, `page_url`, and `title` properties. This is yielded as a block argument.
+    def each_index_group
       # FIXME: there might be a ghost tbody hanging around
       doc = Nokogiri::HTML(open("http://chakoteya.net/StarTrek/index.html"))
       table = doc.css("tbody")
@@ -25,23 +26,49 @@ module StarTrekCLI
     end
         # TODO: FUCKING ENTERPRISE THE GARBAGE SERIESSSS
 
-    def each_table_cell_series
-      doc = Nokogiri::HTML(open("http://chakoteya.net/StarTrek/episodes.htm"))
+    def each_series_page(series_url)
+      doc = Nokogiri::HTML(open(series_url))
       page_rows = doc.css("body > table > tbody > tr")
 
-      page_rows.css("table").each do |table|
+      # FIXME: seasons will have some problems like 101 + 102 or the animated series as season "4"
+
+      page_rows.css("table").each_with_index do |table, index|
         rows = table.css("tr")
         rows.shift
-
         rows.each do |row|
+          link = row.css("td")[0].css("a")
+
           episode_row = {
-            :episode_name => row.css("td")[0].text.strip,
-            :production_date => row.css("td")[1].text.strip,
-            :air_date => row.css("td")[2].text.strip,
+            :season_number => index + 1,
+            :episode_name => link.children.text.strip,
+            :episode_url => link.attr("href").value,
+            :production_number => row.css("td")[1].text.strip.to_i,
+            :air_date => row.css("td")[2].text.strip
           }
+        yield episode_row
         end
       end
-    end
+
+      def episode_page_header(episode_url)
+      # FIXME: change url to variable
+        doc = Nokogiri::HTML(open(episode_url))
+        episode_html = doc.css("body > p > font")
+        binding.pry
+
+        episode_html.each do |info|
+
+            episode_stuff = {
+              :episode_name => info.children[0].text.strip,
+              :star_date => info.children[1].text.strip,
+              :air_date => info.children[3].text.strip
+            }
+          yield episode_stuff
+        end
+      end
+
+
+
+    end # Scraper#each_table_cell_series
 
 
   end # Scraper
