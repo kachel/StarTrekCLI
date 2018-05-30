c8bd8fd57d69bb836debba035b580d1b2befb178
Kachel Wittig <kjwittig@gmail.com>
2018-05-30 02:42:49 -0500

    rearrange cli code

--- lib/StarTrekCLI/controller.rb
+++ lib/StarTrekCLI/controller.rb
@@ -1,46 +1,61 @@
-require "pry"
+require 'pry'
+require 'byebug'
 
 class StarTrekCLI::Controller
 
   def initialize
-     # NYI: something.new
+  # FIXME: yuck
+     StarTrekCLI::Scraper.new.each_index_group do |group|
+       StarTrekCLI::Series.new(group[:title])
+       # StarTrekCLI::Scraper.each_series_page
+     end
+
+     StarTrekCLI::Scraper.new.each_series_page("http://chakoteya.net/StarTrek/episodes.htm") do |row|
+       StarTrekCLI::Season.new(row[:number])
+     end
+
+     StarTrekCLI::Scraper.new.episode_page_header("http://chakoteya.net/DS9/401.htm") do |stuff|
+       StarTrekCLI::Episode.new(stuff[:name])
+     end
   end
 
-  def call
-    puts "Welcome to StarTrekCLI!"
+  # def call
+  #   puts "Welcome to StarTrekCLI!"
+  #
+  #   help
+  #   list_series
+  #
+  #   loop do
+  #     puts "What series would you like to see?"
+  #     command = gets.strip.downcase.to_sym
+  #
+  #     case command
+  #     when :exit
+  #       puts "Goodbye, ensign!"
+  #       break
+  #     when :help
+  #       help
+  #     # when :"list episodes"
+  #     #   list_episodes
+  #     else
+  #       puts "Does not compute, please try again."
+  #     end
+  #   end
+  # end # Controller#call
+  #
+  # def help
+  #   puts "To list all of the Star Trek series, enter 'list series'."
+  #   puts "To quit, type 'exit'."
+  # end
 
-    help
-    list_series
 
-    loop do
-      puts "What series would you like to see?"
-      command = gets.strip.downcase.to_sym
-
-      case command
-      when :exit
-        puts "Goodbye, ensign!"
-        break
-      when :help
-        help
-      # when :"list episodes"
-      #   list_episodes
-      else
-        puts "Does not compute, please try again."
-      end
-    end
-  end # Controller#call
-
-  def help
-    puts "To list all of the Star Trek series, enter 'list series'."
-    puts "To quit, type 'exit'."
-  end
 
   def list_series
     StarTrekCLI::Series.all.each do |series|
       puts "#{series.name}"
     end
   end
-  #
+
   # def list_episodes
   # end
 
