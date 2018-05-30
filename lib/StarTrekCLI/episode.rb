589b423614e58d2332f4371b07d3c0c23b5b26a9
Kachel Wittig <kjwittig@gmail.com>
2018-05-30 04:09:02 -0500

    add @@all to class

--- lib/StarTrekCLI/episode.rb
+++ lib/StarTrekCLI/episode.rb
@@ -4,7 +4,14 @@ require 'byebug'
 class StarTrekCLI::Episode
   attr_reader :name, :air_date, :star_date
 
+  @@all = []
   def initialize(name = nil)
     @name = name
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
 end
