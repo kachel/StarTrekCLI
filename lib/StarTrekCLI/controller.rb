bc5fbcc15308bb665e1b22e93dcd4e380512db32
Kachel Wittig <kjwittig@gmail.com>
2018-05-30 04:08:10 -0500

    streamline controller

--- lib/StarTrekCLI/controller.rb
+++ lib/StarTrekCLI/controller.rb
@@ -21,44 +21,23 @@ class StarTrekCLI::Controller
      end
   end
 
-  # def call
-  #   puts "Welcome to StarTrekCLI!"
-  #
-  #   help
-  #   list_series
-  #
-  #   loop do
-  #     puts "What series would you like to see?"
-  #     command = gets.strip.downcase.to_sym
-  #
-  #     case command
-  #     when :exit
-  #       puts "Goodbye, ensign!"
-  #       break
-  #     when :help
-  #       help
-  #     # when :"list episodes"
-  #     #   list_episodes
-  #     else
-  #       puts "Does not compute, please try again."
-  #     end
-  #   end
-  # end # Controller#call
-  #
-  # def help
-  #   puts "To list all of the Star Trek series, enter 'list series'."
-  #   puts "To quit, type 'exit'."
-  # end
-
-
-
   def list_series
     StarTrekCLI::Series.all.each do |series|
       puts "#{series.name}"
     end
   end
 
-  # def list_episodes
-  # end
+  def list_seasons(series_string)
+    series = StarTrekCLI::Series.find_series_by_name(series_string)
+    series.seasons.each do |season|
+      puts "#{season.number}"
+    end
+  end
+
+  def list_episodes
+    StarTrekCLI::Episode.all.each do |episode|
+      puts "#{episode.name}"
+    end
+  end
 
 end # StarTrekCLI::StarTrekController
