775e1cb9ebec87f692f196fe7454b6ee6564e6d0
Kachel Wittig <kjwittig@gmail.com>
2018-05-31 16:29:21 -0500

    hard code star trek orginal and add case statement to account for random new line in title

--- lib/StarTrekCLI/controller.rb
+++ lib/StarTrekCLI/controller.rb
@@ -7,14 +7,16 @@ class StarTrekCLI::Controller
   def initialize
   # FIXME: yuck
      StarTrekCLI::Scraper.new.each_index_group do |group|
-       StarTrekCLI::Series.new(group[:title])
+      # this is a bad practice but
+      title = group[:title] == "Original\nand Animated Series" ? "Star Trek" : group[:title]
+       StarTrekCLI::Series.new(title)
        # StarTrekCLI::Scraper.each_series_page
      end
 
-    # hard coding to VOY
-     StarTrekCLI::Scraper.new.each_series_page("http://chakoteya.net/Voyager/episode_listing.htm") do |row|
-      series = StarTrekCLI::Series.find_series_by_name("Voyager")
-      StarTrekCLI::Season.new(series, row[:number])
+    # hard coding
+     StarTrekCLI::Scraper.new.each_series_page("http://chakoteya.net/StarTrek/episodes.htm") do |row|
+      series = StarTrekCLI::Series.find_series_by_name("Star Trek")
+      StarTrekCLI::Season.new(series, row[:season_number])
      end
 
      StarTrekCLI::Scraper.new.episode_page_header("http://chakoteya.net/DS9/401.htm") do |stuff|
