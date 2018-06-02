ee704c2e437e2e5871413ef69736e5e3478bda33
Kachel Wittig <kjwittig@gmail.com>
2018-06-01 19:13:39 -0500

    add prod number attr and season attr, change season.episodes from array to hash using prod number, and add raise error to check prod number type

--- lib/StarTrekCLI/episode.rb
+++ lib/StarTrekCLI/episode.rb
@@ -2,14 +2,17 @@ require 'pry'
 require 'byebug'
 
 class StarTrekCLI::Episode
-  attr_reader :name, :air_date, :star_date
+  attr_reader :name, :air_date, :star_date, :production_number, :season
 
   @@all = []
 
-  def initialize(season, number)
+  def initialize(season, production_number, name)
+    @season = season
     @name = name
+    @production_number = production_number
     @@all << self
-    season.episodes[number] = self
+    raise "episode.new was called with an int and not a string" unless production_number.is_a? String
+    season.episodes[production_number] = self
   end
 
   def self.all
